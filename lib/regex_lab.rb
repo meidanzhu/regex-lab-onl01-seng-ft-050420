b77c22b5628bb272a496c68612eeb2c5389a46ed
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 18:58:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,29 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w+/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  array.include[un]
+
+  end
+  return text.scan(/[^un][ing$]/)
 
 end
 
 def words_five_letters_long(text)
+  return text.scan(/\w{5}/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  return nil
 end
 
 def valid_phone_number?(phone)
-
+  return nil
 end
