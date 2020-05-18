04d337cf8a6500a426db3e46e1438defa556564a
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 19:37:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,5 +26,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  return nil
-end
+  if phone.match(/([0-9] *){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/)
+     return true
+  else
+     return false
+  end
+ end
