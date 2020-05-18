2e09edc101c04d956d75e161bff69e89a39935af
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 19:35:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,10 +18,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/A[A-Z]\.?!$/)
-    return false
-  else
+  if text.match(/^[A-Z].*[\.?!]$/)
     return true
+  else
+    return false
   end
 end
 
