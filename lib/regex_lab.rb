fe8ff82f451a7228338bc680dca784cd716ddb8c
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-18 19:08:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,8 +8,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.split(" ")
-  return text.grep(/^un\w+ing$/)
+  word = text.split(" ")
+  return word.grep(/^un\w+ing$/)
 
 end
 
